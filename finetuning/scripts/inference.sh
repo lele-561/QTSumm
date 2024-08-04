8cd435fcaace3b214e06d3d2d1acc9ef60bfa7b4
composer <2789706336@qq.com>
2024-08-04 12:58:03 -0400

    修改inference配置

--- finetuning/scripts/inference.sh
+++ finetuning/scripts/inference.sh
@@ -1,15 +1,20 @@
 export PYTHONPATH=$PYTHONPATH:$(pwd)
-export CUDA_VISIBLE_DEVICES=1,2,3,4
+export CUDA_VISIBLE_DEVICES=0,1
 
-# List of models
+# List of model
 models=(
-  "yale-nlp/flan-t5-large-finetuned-qtsumm"
   "yale-nlp/t5-large-finetuned-qtsumm"
-  "yale-nlp/bart-large-finetuned-qtsumm"
-  "yale-nlp/tapex-large-finetuned-qtsumm"
-  "yale-nlp/reastap-large-finetuned-qtsumm"
-  "yale-nlp/omnitab-large-finetuned-qtsumm"
 )
+
+#models=(
+#  "yale-nlp/flan-t5-large-finetuned-qtsumm"
+#  "yale-nlp/t5-large-finetuned-qtsumm"
+#  "yale-nlp/bart-large-finetuned-qtsumm"
+#  "yale-nlp/tapex-large-finetuned-qtsumm"
+#  "yale-nlp/reastap-large-finetuned-qtsumm"
+#  "yale-nlp/omnitab-large-finetuned-qtsumm"
+#)
+
 
 # Loop over all models
 for model in "${models[@]}"; do
